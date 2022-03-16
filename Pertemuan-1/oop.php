c95b8ad2176bc1ed4a873b7eab769508f6906234
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-03-16 10:58:29 +0700

    Membuat Child Class baru

--- Pertemuan-1/oop.php
+++ Pertemuan-1/oop.php
@@ -20,19 +20,24 @@ class SportBike {
         $this->seat = $seat;
     }
 
+    public function setEngine($engine) {
+        $this->engine = $engine;
+    }
+
+
 }
 
 class CBR extends SportBike {
 
     protected $type, $weight, $color, $price, $brand, $year;
 
-    public function __construct($type, $weight, $color, $price, $brand, $year) {
+    public function __construct($type, $weight, $color, $price, $year) {
         $this->type = $type;
         $this->weight = $weight;
         $this->color = $color;
         $this->price = $price;
-        $this->brand = $brand;
         $this->year = $year;
+        $this->brand = "Honda";
     }
 
     public function setEngine($engine) {
@@ -51,19 +56,67 @@ class CBR extends SportBike {
         return $this->type;
     }
 
+    public function getBrand(){
+        return $this->brand;
+    }
 
 }
 
-$cbr1000 = new CBR('CBR1000', '200 KG', 'Red', 'Rp. 1.000.000.000', 'Honda', '2022');
+class Ninja extends SportBike {
+
+    protected $type, $weight, $color, $price, $brand, $year, $superCharge;
+
+    public function __construct($type, $weight, $color, $price, $year) {
+        $this->type = $type;
+        $this->weight = $weight;
+        $this->color = $color;
+        $this->price = $price;
+        $this->year = $year;
+        $this->brand = "Honda";
+        $this->superCharge = false;
+    }
+
+    public function setEngine($engine) {
+        $this->engine = $engine;
+    }
+    
+    public function setsuperCharge() {
+        $this->superCharge = true;
+    }
+
+   public function getSeat() {
+        return $this->seat;
+    }
+
+    public function getEngine() {
+        return $this->engine;
+    }
+
+    public function getType(){
+        return $this->type;
+    }
+
+    public function getBrand(){
+        return $this->brand;
+    }
+
+
+}
+
+// Membuat Object Dari Class CBR
+// Obj Pertama
+$cbr1000 = new CBR('CBR1000', '200 KG', 'Red', 'Rp. 1.000.000.000', '2022');
 $cbr1000->setEngine('1000 CC');
 $cbr1000->setFairing('Full Fairing');
-$cbr1000->setSeat('2 Seats');
+
 
 echo $cbr1000->getType();
 echo "<br>";
 echo $cbr1000->getEngine();
 echo "<br>";
-echo $cbr1000->getSeat();
+echo $cbr1000->getBrand();
+echo "<br><hr>";
+
 
 
 ?>
