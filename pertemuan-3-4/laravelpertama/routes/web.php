ea0dbfb5f464e5c9ddbeaee10f50bed38770bfa0
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-04-04 20:51:06 +0700

    Pertemuan Ke - 4 (finished)

--- pertemuan-3-4/laravelpertama/routes/web.php
+++ pertemuan-3-4/laravelpertama/routes/web.php
@@ -22,9 +22,7 @@ Route::get('/coba', function () {
     return view('coba');
 });
 
-Route::get('/coba/{no}', function ($no) {
-    return "Coba Ke - " . $no;
-});
+Route::get('/coba/{no}',[CobaController::class, 'coba']);
 
 Route::get('/test', [CobaController::class, 'index']);
 
