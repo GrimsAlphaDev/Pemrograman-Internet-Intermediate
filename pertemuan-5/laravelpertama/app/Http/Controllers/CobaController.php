ce810b13bf7f0a62b110bb87b8b8b48590daa9b5
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-04-12 22:25:05 +0700

    Pertemuan 5 (finish)

--- pertemuan-5/laravelpertama/app/Http/Controllers/CobaController.php
+++ pertemuan-5/laravelpertama/app/Http/Controllers/CobaController.php
@@ -2,26 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Friends;
 use Illuminate\Http\Request;
 
 class CobaController extends Controller
 {
-    public function index(){
-        return "Test Berhasil";
+    // public function index(){
+    //     return "Test Berhasil";
+    // }
+    
+    // public function urutan($ke){
+
+    //     $friends = Friends::paginate(2);
+
+    //     return view('friend', compact('friends'));
+    // }
+
+    // public function coba($ke){
+    //     return view('coba', ['ke' => $ke]);
+    // }
+
+    public function index()
+    {
+        $friends = Friends::paginate(2);
+
+        return view('index', compact('friends'));
     }
     
-    public function urutan($ke){
-
-        $numbers = [
-            ['ke' => $ke, 'nomor' => 20],
-            ['ke' => $ke, 'nomor' => 30],
-            ['ke' => $ke, 'nomor' => 40],
-        ];
-
-        return view('urutan', compact('numbers'));
+    public function create()
+    {
+        return view('create');
+    }
+    
+    public function store(Request $request)
+    {
+        // Validate the request...
+ 
+        $friends = new Friends();
+ 
+        $friends->nama = $request->nama;
+        $friends->no_telp = $request->no_tlp;
+        $friends->alamat = $request->alamat;
+ 
+        $friends->save();
     }
 
-    public function coba($ke){
-        return view('coba', ['ke' => $ke]);
-    }
 }
